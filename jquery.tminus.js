0498d3d15818f0ce684b80081064780f8630a46b
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2014-09-25 14:49:19 -0400

    fixing issue with time formatting

--- jquery.tminus.js
+++ jquery.tminus.js
@@ -1,15 +1,15 @@
  /**********************************
- * 
+ *
  *  Rockhome's tminus
  *  Version 1.1
- *  
+ *
  *  Author: Mike Mackintosh
  *  Email: m [at] rocketho [dot] me
  *  Date: 20140224
- * 
+ *
  **********************************/
  (function( $ ) {
-    $.fn.rocketLaunch = function( options ) {
+    $.fn.tminus = function( options ) {
 
       // Default Settings:
       var defaults = {
@@ -33,6 +33,7 @@
           setTimeout(function() { facechange(target); }, 500);
 
           $.each(getTime(opts.tminus), function(v, k){
+
             if (k > 1){
               v = v+'s'
             }
@@ -60,26 +61,49 @@
       }
 
       function getTime(date){
-          if (date) {
-              var e = new Date(date);
-              var b = new Date();
-              var d = new Date(e.getTime() - b.getTime());
-          } else
-              var d = new Date();
-              var t = pad(date ? d.getFullYear() - 70 : d.getFullYear())
-                  + '' + pad(date ? d.getMonth() : d.getMonth() + 1)
-                  + '' + pad(date ? d.getDate() - 1 : d.getDate())
-                  + '' + pad(d.getHours())
-                  + '' + pad(d.getMinutes())
-                  + '' + pad(d.getSeconds());
-          return {
-              'year': t.charAt(2)+t.charAt(3),
-              'month': t.charAt(4)+t.charAt(5),
-              'day': t.charAt(6)+t.charAt(7),
-              'hour': t.charAt(8)+t.charAt(9),
-              'minute': t.charAt(10)+t.charAt(11),
-              'second': t.charAt(12)+t.charAt(13),
-          };
+        if (date) {
+            var e = new Date(date);
+            var b = new Date();
+            var d = (new Date(e.getTime() - b.getTime())) / 1000;
+        }
+        else
+            var d = new Date();
+
+        // get total seconds between the times
+        var delta = Math.abs(e.getTime() - b.getTime()) / 1000;
+
+        // calculate (and subtract) whole years
+        var years = Math.floor(delta / 31557600);
+        delta -= years * 31557600;
+        
+        // calculate (and subtract) whole months
+        var months = Math.floor(delta / 2592000);
+        delta -= months * 2592000;
+
+        // calculate (and subtract) whole days
+        var days = Math.floor(delta / 86400);
+        delta -= days * 86400;
+
+        // calculate (and subtract) whole hours
+        var hours = Math.floor(delta / 3600) % 24;
+        delta -= hours * 3600;
+
+        // calculate (and subtract) whole minutes
+        var minutes = Math.floor(delta / 60) % 60;
+        delta -= minutes * 60;
+
+        // what's left is seconds
+        var seconds = Math.floor(delta);
+
+        return {
+          'year': pad(years),
+          'month': pad(months),
+          'day': pad(days),
+          'hour': pad(hours),
+          'minute': pad(minutes),
+          'second': pad(seconds),
+        };
+
       }
 
     };
